0303f31901b9d16f8375b6c409eda2da3330d741
Your Name <you@example.com>
2026-02-02 14:40:44 -0500

    Create ngnix log folder

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 set -e
 
-# 1. Create the supervisor log directory (Fixes your error)
 mkdir -p /var/log/supervisor
+mkdir -p /var/log/nginx
 
-# 2. Check if repo needs initialization (Fixes volume mount issues)
-# We check if config.yml exists to see if the repo is initialized
+
 if [ ! -f "/srv/fdroid/config.yml" ]; then
     echo "Initializing F-Droid repository..."
     python3 /app/init_repo.py
@@ -13,5 +12,4 @@ else
     echo "F-Droid repository already initialized."
 fi
 
-# 3. Hand over control to the CMD (Supervisord)
 exec "$@"
